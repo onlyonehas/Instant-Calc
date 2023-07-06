4871638cebf792c479c7ded2cd02188a0b245dfd
Hassan Abbas <hassanabbas@hassans-air.home>
2023-07-07 00:17:48 +0100

    feat: update to accept all math expressions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, ChangeEvent } from 'react';
 import styles from '../styles/Home.module.css';
+import { evaluate } from 'mathjs';
 
 interface VariableMap {
   [name: string]: number;
@@ -8,19 +9,19 @@ interface VariableMap {
 const initialInput = `# Expenses
 //house: 300
 britishgas: 320
-pck: 200
-food: 250
+pck: 200 
+food: 250 
 kitchen: 85
-fuel/out: 150
+fuel/out: 150 
 phone: 50+32+8
 arabic+quran: 88
 travel: 80
-wahed: 50
+wahed: 50 
 utl: 85
-Expense= sum
+Expenses = sum 
 Result = 2,964 - prev
 Now = 1000
-preExpense= Now + Result
+preExpense= Now + Result 
 postExpense= Now - expenses`;
 
 export default function Home() {
@@ -28,7 +29,12 @@ export default function Home() {
   const [output, setOutput] = useState('');
   const [sum, setSum] = useState(0);
   const [prev, setPrev] = useState(0);
-  const [result, setResult] = useState(0);
+  const reservedKeywords = ["prev", "sum"]
+  const variables: VariableMap = {};
+  let newOutput = '';
+  let tempSum = 0;
+  let tempPrev = 0;
+
 
   useEffect(() => {
     handleInput();
@@ -40,79 +46,71 @@ export default function Home() {
 
   const handleInput = () => {
     const lines = input.split('\n');
-    const variables: VariableMap = {};
-  
-    let newOutput = '';
-    let tempSum = 0;
-    let tempPrev = 0;
-    let tempResult = 0;
-  
+    // let tempResult = 0;
+
     lines.forEach((line) => {
       const trimmedLine = line.trim();
-  
+      let result = 0
       if (trimmedLine.startsWith('//')) {
         // Commented line, skip evaluation
-        newOutput += line + '\n';
+        newOutput += '-- \n';
       } else if (trimmedLine.includes(':')) {
-        const [name, expression] = trimmedLine.split(':').map((item) => item.trim());
-        const result = evaluateExpression(expression, variables);
+        const [name, expression] = trimmedLine.split(':').map((item) => item.trim().toLowerCase());
+        result = evaluateExpression(expression, variables)
         variables[name] = result;
-        newOutput += `${result}\n`;
-        tempSum += result;
       } else if (trimmedLine.includes('=')) {
-        const [name, expression] = trimmedLine.split('=').map((item) => item.trim());
-        if (name === 'prev') {
-          tempPrev = tempSum;
-          variables[name] = tempPrev;
-        } else if (name === 'Expense') {
-          variables[name] = tempSum; // Set Expense to the calculated sum
-          newOutput += `${tempSum}\n`;
-        } else if (name === 'Result') {
-          tempResult = 2964 - tempPrev;
-          variables[name] = tempResult;
-          newOutput += `${tempResult}\n`;
-        } else {
-          const result = evaluateExpression(expression, variables);
-          variables[name] = result;
-          newOutput += `${result}\n`;
-        }
-      } else if (trimmedLine === 'sum') {
-        newOutput += `${tempSum}\n`;
+        const [name, expression] = trimmedLine.split("=").map((item) => item.trim().replace(/\,/g, '').toLowerCase());
+        result = evaluateExpression(expression, variables);         
+        variables[name] = result;
       } else {
-        const result = evaluateExpression(trimmedLine, variables);
-        variables[trimmedLine] = result;
-        newOutput += `${result}\n`;
+        if (trimmedLine) {
+          result = evaluateExpression(trimmedLine, variables);
+        }
       }
+      console.log(result)
+      newOutput += `${result? result : '--'}\n`;
+      tempSum += result;
+      tempPrev = result;
     });
-  
+
     setOutput(newOutput);
     setSum(tempSum);
     setPrev(tempPrev);
-    setResult(tempResult);
+    // setResult(tempResult);
   };
-  
+
   const evaluateExpression = (expression: string, variables: VariableMap) => {
-    const parts = expression.split('+').map((part) => part.trim());
-    let result = 0;
+    const individualValue = expression.split(/[+-/*()]/gm).map((item) => item.trim());
+    // let result2 = evaluateExpression(moreExpressions, variables);
+    individualValue.forEach(value => {
+      if (typeof value == "string") {
+        if (reservedKeywords.includes(value)) {
+          if (value == "prev") {
+            expression = expression.replace(value, String(tempPrev))
+          }
+          if (value == "sum") {
+            expression = expression.replace(value, String(tempSum))
+          }
+        }
+        
+        if (variables[value] >= 0) {
+          expression = expression.replace(value, String(variables[value]))
+        }
+      }
+    })
 
-    parts.forEach((part) => {
-      const value = variables[part] || parseFloat(part);
-      result += value || 0;
-    });
+    let res
+    const exp  = Number(expression) 
 
-    return result;
-  };
-
-  const calculateSum = (variables: VariableMap) => {
-    let sum = 0;
-
-    for (const variable in variables) {
-      if (typeof variables[variable] === 'number') {
-        sum += variables[variable];
+    if(expression.length > 0){
+      try{
+        res = evaluate(expression)
+      }catch(e){
+        console.log(e)
       }
     }
 
-    return sum;
+    return res | exp
   };
 
   return (
